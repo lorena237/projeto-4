643384ba62b48e24a8ccd2945f9791e289877269
lorena237 <134697667+lorena237@users.noreply.github.com>
2024-09-03 21:03:28 -0300

    aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,78 @@
+export const perguntas = [
+    {
+        enunciado: "A heroína da nossa história Elara, uma aprendiz de guardiã dos cristais. Elara sonhava em se tornar uma grande protetora do equilíbrio entre a luz e a sombra, como sua avó antes dela. Certo dia, enquanto explorava as antigas ruínas de um templo esquecido, Elara encontrou um misterioso mapa que prometia revelar o segredo para restaurar a harmonia  completa no reino, ameaçado por uma crescente escuridão. Elara decide seguir o mapa. Antes de partir, ela deve escolher um companheiro para a jornada:",
+        alternativas: [
+            {
+                texto: "A sábia coruja Lyria, que possui vasto conhecimento sobre o reino.",
+                afirmacao: [
+                   
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "O corajoso guerreiro Kael, famoso por sua bravura e habiloidade com a espada.",
+                afirmacao: [
+                   
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado:"Durante a viajem, Elara e seu companheiro encontram um desfiladeiro. Há duas maneiras de atravessá-lo:",
+        alternativas: [
+            {
+                texto:  "Seguir por uma ponte estreita que parece prestes a desmoronar.",
+                afirmacao: [
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Descer até o fundo do desfiladeiro e enfrentar as criaturas que lá habitam.",
+                afirmacao: [
+                   
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado:"No meio da floresta sombria, eles encontram um ser enigmático que oferece ajuda em troca de algo precioso.",
+        alternativas: [
+            {
+                texto: "Aceitar a ajuda e dar ao ser o cristal de sua alma.",
+                afirmacao: [
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto:"recusar a ajuda e continuar sozinha, confiando apenas em si mesma.",
+                afirmacao: [
+
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Finalmente, ele chegam ao local indicado pelo mapa, mas encontram uma barreira mágica. Para atravessá-la, Elara deve:",
+        alternativas: [
+            {
+                texto: "Recitar um antigo encantamento de luz.",
+                afirmacao: [
+                    
+                ],
+                proxima: 5,
+            },
+            {
+                texto:  "Usar um artefato de sombra encontrado nas ruínas do templo",
+                afirmacao: [
+
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+];
